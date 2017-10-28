33dbc86194aa853f77ddb69f6708efdbac0a4f54
Farhana <farhana.wafa@yahoo.com>
2017-10-28 22:21:55 +0300

    production add database

--- farhana/settings/production.py
+++ farhana/settings/production.py
@@ -90,10 +90,10 @@ WSGI_APPLICATION = 'farhana.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 import dj_database_url
