d5bb9c8332469069c530847f0b146666072c7516
Farhana <farhana.wafa@yahoo.com>
2017-10-28 22:44:23 +0300

    fixed errors with profiles

--- profiles/models.py
+++ profiles/models.py
@@ -67,7 +67,7 @@ def post_save_user_receiver(sender, instance, created, *args, **kwargs):
         profile, is_created = Profile.objects.get_or_create(user=instance)
         default_user_profile = Profile.objects.get_or_create(user__id=1)[0] #user__username=
         default_user_profile.followers.add(instance)
-        profile.followers.add(default_user_profile.user)
-        profile.followers.add(2)
+        # profile.followers.add(default_user_profile.user)
+        # profile.followers.add(2)
 
 post_save.connect(post_save_user_receiver, sender=User)
